e701da4554163b54194cc710a0c792d1a26a558b
Abir Arafat Chawdhury <abirxdhackz.info.me@gmail.com>
2025-02-08 12:16:25 +0600

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+# Pyrogram setup
+API_ID = "123456"  # Replace this API ID with your actual API ID
+API_HASH = "12123456ABCDEFGHI  # Replace this API HASH with your actual API HASH
+BOT_TOKEN = "12345678:XXXXXXXXXXXXXXXX"  # Replace this BOT_TOKEN
+
+# Bot USER_NAME
+BOT_USERNAME = "CcZxdvvtfBot" # Dont Use @ Just Put the bot username
